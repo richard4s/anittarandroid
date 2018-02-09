261ea33a25486e3493a88533ccce92858146c6eb
Richard.oluwo <richard.oluwo@imaxinacion.info>
2018-02-09 13:45:28 +0100

    Version 1.0 at Initial commit

---
+++ app/src/main/java/com/example/imaxinacion/anittar_android/welcome_screen.java
@@ -0,0 +1,13 @@
+package com.example.imaxinacion.anittar_android;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class welcome_screen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome_screen);
+    }
+}
